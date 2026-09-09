4760b285c6fc6702473f21e1d307eaa119ba9224
Ahmed Bahgat <72673873+AhmedBahgat010@users.noreply.github.com>
2026-09-09 04:12:19 +0300

    Time: 2 ms (68.4%), Space: 85 MB (89.62%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,23 +1,14 @@
 class Solution {
+   public boolean isMonotonic(int[] nums) {
 
-    public boolean isMonotonic(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return false;
-        }
-        if (nums.length == 1) {
-            return true;
-        }
         boolean increasing = false;
         boolean decreasing = false;
-
         for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                continue;
-            }
-            if (nums[i] <= nums[i + 1]) {
+
+            if (nums[i] < nums[i + 1]) {
                 increasing = true;
             }
-            if (nums[i] >= nums[i + 1]) {
+            if (nums[i] > nums[i + 1]) {
                 decreasing = true;
             }
         }
